1ea192ea6451d3416dcc120e183bcdd02bf4438b
nzluis <luisnavarrofs@gmail.com>
2025-03-16 18:35:50 +0100

    Add api url difference with parameter

--- react/src/utils/getHost.js
+++ react/src/utils/getHost.js
@@ -1,6 +1,12 @@
 const getHost = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('api')) {
+        return 'https://xnqz3kpy44.execute-api.eu-west-3.amazonaws.com/v1';
+    } else {
+        return 'https://fantasygpback.onrender.com';
+    }
     // return "http://localhost:5000";
-    return 'https://fantasygpback.onrender.com';
+    // return 'https://fantasygpback.onrender.com';
     // return 'https://xnqz3kpy44.execute-api.eu-west-3.amazonaws.com/v1';
 };
 export default getHost;
