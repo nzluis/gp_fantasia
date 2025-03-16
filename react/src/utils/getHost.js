81942a67bafbab96be0090eaef2482976440203d
nzluis <luisnavarrofs@gmail.com>
2025-03-16 20:16:01 +0100

    Revert aws as main api

--- react/src/utils/getHost.js
+++ react/src/utils/getHost.js
@@ -1,12 +1,9 @@
 const getHost = () => {
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has('api')) {
-        return 'https://xnqz3kpy44.execute-api.eu-west-3.amazonaws.com/v1';
-    } else {
         return 'https://fantasygpback.onrender.com';
+    } else {
+        return 'https://xnqz3kpy44.execute-api.eu-west-3.amazonaws.com/v1';
     }
-    // return "http://localhost:5000";
-    // return 'https://fantasygpback.onrender.com';
-    // return 'https://xnqz3kpy44.execute-api.eu-west-3.amazonaws.com/v1';
 };
 export default getHost;
