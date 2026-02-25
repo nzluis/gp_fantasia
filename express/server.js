8b537e044a3fa0df2a3a8b1b6265af1d147fd45c
nzluis <luisnavarrofs@gmail.com>
2026-02-25 21:11:53 +0100

    Change db for season26

--- express/server.js
+++ express/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 // Conectar a MongoDB
 mongoose.connect(
-    `mongodb+srv://gp_user:${process.env.MONGO_PASS}@cluster0.cd0w6.mongodb.net/testing`
+    `mongodb+srv://gp_user:${process.env.MONGO_PASS}@cluster0.cd0w6.mongodb.net/season26`
 );
 
 const riderSchema = new mongoose.Schema({
